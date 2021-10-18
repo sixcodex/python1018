b39c41f5d5104f2fb9121b70af3a5709d5844eb7
sixcodex <sixcodex@hanwha.com>
2021-10-18 15:28:31 +0900

    교집합 함수연습1

--- DemoDict.py
+++ DemoDict.py
@@ -18,3 +18,18 @@ for k,v in device.items():
 phone = {"kim":"111", "lee":"2222"}
 p=phone
 print(p)
+
+#교집합을 구하는 함수
+def intersect(prelist, postlist):
+    #함수내부에만 있는 지역변수
+    result=[]
+    #단어 글자를 슬라이싱 하는 코드 H(0)|A(1)|M(2)
+    for x in prelist:
+        #postlist에도 있고 result에는 없는 글자면 추가
+        if x in postlist and x not in result:
+            result.append(x)
+    #루프 다돌고 나면 Return
+    return result
+
+#함수 호출
+print( intersect("HAM", "SPAM"))
